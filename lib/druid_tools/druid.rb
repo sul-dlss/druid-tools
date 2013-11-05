c1302fecec429843f303782c019052bbbd6b8bdf
Willy Mene <wmene@stanford.edu>
2013-11-05 12:22:04 -0800

    Add prune method

--- lib/druid_tools/druid.rb
+++ lib/druid_tools/druid.rb
@@ -3,10 +3,10 @@ require 'pathname'
 module DruidTools
   class Druid
     attr_accessor :druid, :base
-    
+
     class << self
       attr_accessor :prefix
-      
+
       # @return [Regexp] matches druid:aa111aa1111 or aa111aa1111
       def pattern
         /\A(?:#{self.prefix}:)?([a-z]{2})(\d{3})([a-z]{2})(\d{4})\z/
@@ -25,19 +25,19 @@ module DruidTools
 
     end
     self.prefix = 'druid'
-    
+
     [:content, :metadata, :temp].each do |dir_type|
       self.class_eval <<-EOC
         def #{dir_type}_dir(create=true)
           path("#{dir_type}",create)
         end
-        
+
         def find_#{dir_type}(path)
           find(:#{dir_type},path)
         end
       EOC
     end
-    
+
     # @param druid [String] A valid druid
     # @param base [String] The directory used by #path
     def initialize(druid, base='.')
@@ -49,21 +49,21 @@ module DruidTools
       @base = base
       @druid = druid
     end
-  
+
     def id
       @druid.scan(self.class.pattern).flatten.join('')
     end
-  
+
     def tree
       @druid.scan(self.class.pattern).flatten + [id]
     end
-  
+
     def path(extra=nil, create=false)
       result = File.join(*([base,tree,extra].compact))
       mkdir(extra) if create and not File.exists?(result)
       result
     end
-    
+
     def mkdir(extra=nil)
       new_path = path(extra)
       if(File.symlink? new_path)
@@ -74,7 +74,7 @@ module DruidTools
       end
       FileUtils.mkdir_p(new_path)
     end
-    
+
     def find(type, path)
       possibles = [self.path(type.to_s),self.path,File.expand_path('..',self.path)]
       loc = possibles.find { |p| File.exists?(File.join(p,path)) }
@@ -107,7 +107,7 @@ module DruidTools
       FileUtils.mkdir_p(real_path)
       FileUtils.ln_s(source, new_path, :force=>true)
     end
-  
+
     def rmdir(extra=nil)
       parts = tree
       parts << extra unless extra.nil?
@@ -121,6 +121,34 @@ module DruidTools
         end
         parts.pop
       end
+    end
+
+    def pathname
+      Pathname self.path
+    end
+
+    def base_pathname
+      Pathname self.base
+    end
+
+    def prune!
+      this_path = pathname
+      parent = this_path.parent
+      parent.rmtree if parent.exist? && parent != base_pathname
+      prune_ancestors parent.parent
+    end
+
+  protected
+
+    # @param [Pathname] outermost_branch The branch at which pruning begins
+    # @return [void] Ascend the druid tree and prune empty branches
+    def prune_ancestors(outermost_branch)
+      while outermost_branch.children.size == 0
+        outermost_branch.rmdir
+        outermost_branch = outermost_branch.parent
+        break if  outermost_branch == base_pathname
+      end
+    rescue
     end
 
   end
--- spec/druid_tools_spec.rb
+++ spec/druid_tools_spec.rb
@@ -1,6 +1,6 @@
-#require File.expand_path('../spec_helper',__FILE__)
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 #puts $LOAD_PATH.join("\n")
-require 'druid-tools'
+require 'tmpdir'
 
 describe DruidTools::Druid do
   before (:all) do
@@ -11,7 +11,7 @@ describe DruidTools::Druid do
     @druid_2 = 'druid:cd456gh1234'
     @tree_2 = File.join(@fixture_dir,'cd/456/gh/1234/cd456gh1234')
   end
-  
+
   after(:each) do
     FileUtils.rm_rf(File.join(@fixture_dir,'cd'))
   end
@@ -48,23 +48,23 @@ describe DruidTools::Druid do
     DruidTools::Druid.new('druid:cd456ef7890',@fixture_dir).druid.should == 'druid:cd456ef7890'
     DruidTools::Druid.new('cd456ef7890',@fixture_dir).druid.should == 'druid:cd456ef7890'
   end
-  
+
   it "should extract the ID from the stem" do
     DruidTools::Druid.new('druid:cd456ef7890',@fixture_dir).id.should == 'cd456ef7890'
     DruidTools::Druid.new('cd456ef7890',@fixture_dir).id.should == 'cd456ef7890'
   end
-  
+
   it "should raise an exception if the druid is invalid" do
     lambda { DruidTools::Druid.new('nondruid:cd456ef7890',@fixture_dir) }.should raise_error(ArgumentError)
     lambda { DruidTools::Druid.new('druid:cd4567ef890',@fixture_dir) }.should raise_error(ArgumentError)
   end
-  
+
   it "should build a druid tree from a druid" do
     druid = DruidTools::Druid.new(@druid_1,@fixture_dir)
     druid.tree.should == ['cd','456','ef','7890','cd456ef7890']
     druid.path.should == @tree_1
   end
-  
+
   it "should create and destroy druid directories" do
     File.exists?(@tree_1).should be_false
     File.exists?(@tree_2).should be_false
@@ -79,7 +79,7 @@ describe DruidTools::Druid do
     druid_2.mkdir
     File.exists?(@tree_1).should be_true
     File.exists?(@tree_2).should be_true
-    
+
     druid_2.rmdir
     File.exists?(@tree_1).should be_true
     File.exists?(@tree_2).should be_false
@@ -94,7 +94,7 @@ describe DruidTools::Druid do
     before :all do
       DruidTools::Druid.prefix = 'sulair'
     end
-    
+
     after :all do
       DruidTools::Druid.prefix = 'druid'
     end
@@ -105,14 +105,14 @@ describe DruidTools::Druid do
       DruidTools::Druid.new('cd456ef7890',@fixture_dir).druid.should == 'sulair:cd456ef7890'
     end
   end
-  
+
   describe "content directories" do
     it "should know where its content goes" do
       druid = DruidTools::Druid.new(@druid_1,@fixture_dir)
       druid.content_dir(false).should  == File.join(@tree_1,'content')
       druid.metadata_dir(false).should == File.join(@tree_1,'metadata')
       druid.temp_dir(false).should     == File.join(@tree_1,'temp')
-    
+
       File.exists?(File.join(@tree_1,'content')).should be_false
       File.exists?(File.join(@tree_1,'metadata')).should be_false
       File.exists?(File.join(@tree_1,'temp')).should be_false
@@ -123,31 +123,31 @@ describe DruidTools::Druid do
       druid.content_dir.should  == File.join(@tree_1,'content')
       druid.metadata_dir.should == File.join(@tree_1,'metadata')
       druid.temp_dir.should     == File.join(@tree_1,'temp')
-    
+
       File.exists?(File.join(@tree_1,'content')).should be_true
       File.exists?(File.join(@tree_1,'metadata')).should be_true
       File.exists?(File.join(@tree_1,'temp')).should be_true
     end
-  
+
     it "should match glob" do
       druid = DruidTools::Druid.new(@druid_1,@fixture_dir)
       druid.mkdir
       Dir.glob(File.join(File.dirname(druid.path), DruidTools::Druid::glob)).size.should == 1
     end
   end
-  
+
   describe "content discovery" do
     before :all do
       @druid = DruidTools::Druid.new(@druid_1,@fixture_dir)
       @filelist = %w(1 2 3 4).collect { |num| "someFile#{num}" }
     end
-    
+
     it "should find content in content directories" do
       location = @druid.content_dir
       File.open(File.join(location,'someContent'),'w') { |f| f.write 'This is the content' }
       @druid.find_content('someContent').should == File.join(location,'someContent')
     end
-    
+
     it "should find content in the root directory" do
       location = @druid.path(nil,true)
       File.open(File.join(location,'someContent'),'w') { |f| f.write 'This is the content' }
@@ -159,7 +159,7 @@ describe DruidTools::Druid do
       File.open(File.join(location,'someContent'),'w') { |f| f.write 'This is the content' }
       @druid.find_content('someContent').should == File.join(location,'someContent')
     end
-    
+
     it "should not find content in the wrong content directory" do
       location = @druid.metadata_dir
       File.open(File.join(location,'someContent'),'w') { |f| f.write 'This is the content' }
@@ -208,48 +208,102 @@ describe DruidTools::Druid do
     end
 
   end
-  
+
   describe "#mkdir error handling" do
     it "raises SameContentExistsError if the directory already exists" do
       druid_2 = DruidTools::Druid.new(@druid_2,@fixture_dir)
       druid_2.mkdir
       lambda { druid_2.mkdir }.should raise_error(DruidTools::SameContentExistsError)
     end
-    
+
     it "raises DifferentContentExistsError if a link already exists in the workspace for this druid" do
       source_dir = '/tmp/content_dir'
-      FileUtils.mkdir_p(source_dir)      
+      FileUtils.mkdir_p(source_dir)
       dr = DruidTools::Druid.new(@druid_2,@fixture_dir)
       dr.mkdir_with_final_link(source_dir)
       lambda { dr.mkdir }.should raise_error(DruidTools::DifferentContentExistsError)
     end
   end
-  
+
   describe "#mkdir_with_final_link" do
-    
+
     before(:each) do
       @source_dir = '/tmp/content_dir'
-      FileUtils.mkdir_p(@source_dir)      
+      FileUtils.mkdir_p(@source_dir)
       @dr = DruidTools::Druid.new(@druid_2,@fixture_dir)
     end
-    
+
     it "creates a druid tree in the workspace with the final directory being a link to the passed in source" do
       @dr.mkdir_with_final_link(@source_dir)
 
       File.should be_symlink(@dr.path)
       File.readlink(@tree_2).should == @source_dir
     end
-    
+
     it "should not error out if the link to source already exists" do
       @dr.mkdir_with_final_link(@source_dir)
       File.should be_symlink(@dr.path)
       File.readlink(@tree_2).should == @source_dir
     end
-    
+
     it "raises DifferentContentExistsError if a directory already exists in the workspace for this druid" do
       @dr.mkdir(@fixture_dir)
       lambda { @dr.mkdir_with_final_link(@source_di) }.should raise_error(DruidTools::DifferentContentExistsError)
     end
   end
-  
+
+  describe "#prune!" do
+
+    let(:workspace) { Dir.mktmpdir }
+
+    let(:dr1) { DruidTools::Druid.new @druid_1, workspace }
+    let(:dr2) { DruidTools::Druid.new @druid_2, workspace }
+    let(:pathname1) { dr1.pathname }
+
+    after(:each) do
+      FileUtils.remove_entry workspace
+    end
+
+    context "shared ancestor" do
+
+      before(:each) do
+        dr1.mkdir
+        dr2.mkdir
+        dr1.prune!
+      end
+
+      it "deletes the outermost directory" do
+        expect(File).to_not exist(dr1.path)
+      end
+
+      it "deletes empty ancestor directories" do
+        expect(File).to_not exist(pathname1.parent)
+        expect(File).to_not exist(pathname1.parent.parent)
+      end
+
+      it "stops at ancestor directories that have children" do
+        # 'cd/456' should still exist because of druid2
+        shared_ancestor = pathname1.parent.parent.parent
+        expect(shared_ancestor.to_s).to match(/cd\/456$/)
+        expect(File).to exist(shared_ancestor)
+      end
+    end
+
+    it "removes all directories up to the base path when there are no common ancestors" do
+      dr1.mkdir
+      dr1.prune!
+      expect(File).to_not exist(File.join(workspace, 'cd'))
+      expect(File).to exist(workspace)
+    end
+
+    it "removes directories with symlinks" do
+      source_dir = File.join workspace, 'src_dir'
+      FileUtils.mkdir_p(source_dir)
+      dr2.mkdir_with_final_link(source_dir)
+      dr2.prune!
+      expect(File).to_not exist(dr2.path)
+      expect(File).to_not exist(File.join(workspace, 'cd'))
+    end
+  end
+
 end
