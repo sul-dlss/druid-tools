3ceab41a8bcc1dff75b072158be27db812442ad5
Naomi Dushay <ndushay@stanford.edu>
2017-10-16 18:39:11 -0700

    spec_helper: explicitly remove spec from coverage stats

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,13 @@
 #$LOAD_PATH.unshift(File.expand_path('../../lib',__FILE__))
 
+require 'coveralls'
+Coveralls.wear_merged! # because we run travis on multiple rubies
+
+require 'simplecov'
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'spec'
+end
+
 require 'bundler/setup'
 Bundler.require(:default, :development)
-
-require 'coveralls'
-Coveralls.wear!
